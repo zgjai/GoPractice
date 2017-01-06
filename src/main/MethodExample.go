02a29346a80ad5d69bd74c52b7349449863ea426
zgjai <18710057269@163.com>
2017-01-06 11:20:57 +0800

    method

---
+++ src/main/MethodExample.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+type coder struct {
+	name      string
+	languages []string
+}
+
+func sayName(c coder) {
+	fmt.Println("my name is: ", c.name)
+}
+
+//coder类型的method，接收者类型为coder
+func (c coder) sayName() {
+	fmt.Println("my name is: ", c.name)
+}
+
+//coder类型的method，接收者类型为coder
+func (c coder) learnLanguageUnknown(language string) {
+	fmt.Println("learn more language: ", language)
+	c.languages = append(c.languages, language)
+}
+
+//coder类型的method，接收者类型为*coder
+func (c *coder) learnLanguage(language string) {
+	fmt.Println("learn more language: ", language)
+	c.languages = append(c.languages, language)
+}
+
+func main() {
+	c := coder{"Tom", []string{"java"}}
+	fmt.Println(c)
+	fmt.Println("********我是分割线********")
+
+	sayName(c)
+	c.sayName()
+	fmt.Println("********我是分割线********")
+
+	c.learnLanguageUnknown("Go")
+	fmt.Println(c)
+	fmt.Println("********我是分割线********")
+
+	c.learnLanguage("Go")
+	fmt.Println(c)
+	fmt.Println("********我是分割线********")
+
+	i := newInt(1)
+	j := newInt(2)
+	fmt.Println(i.compareNewInt(j))
+}
+
+//为基本类型添加自定义method
+type newInt int
+
+func (i newInt) compareNewInt(j newInt) bool {
+	if i > j {
+		return true
+	} else {
+		return false
+	}
+}
